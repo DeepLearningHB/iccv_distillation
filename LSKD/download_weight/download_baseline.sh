82d1f948f50a193a7d743bbf233d4872a1d81bd5
Hanbeen Lee <37934236+DeepLearningHB@users.noreply.github.com>
2021-03-22 21:40:01 +0900

    Add files via upload

---
+++ LSKD/download_weight/download_baseline.sh
@@ -0,0 +1,19 @@
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214849&authkey=ABThVFL-QvsS4cI" -O ../weights/resnet32x4_best.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214850&authkey=ANAhyUqSL8QhP9c" -O ../weights/resnet8x4_best.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214852&authkey=AKDkXoiwASVaFEA" -O ../weights/resnet8x4_attention.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214854&authkey=AE5dbLgjqfjciM4" -O ../weights/resnet8x4_crd.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214851&authkey=AB9V4SEgjvOZePE" -O ../weights/resnet8x4_rkd.pth
+
+wget --no-check-certificate  "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214858&authkey=ANHerewEuf4qzfY" -O ../weights/vgg19_best.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214857&authkey=ANG0W_grKXkEN-c" -O ../weights/vgg11_best.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214853&authkey=ADbBNdYW8rnY2aQ" -O ../weights/vgg11_attention.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214856&authkey=AJYEoiRPCM6dGjI" -O ../weights/vgg11_crd.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214855&authkey=AGwapYtLguF8cO8" -O ../weights/vgg11_rkd.pth
---
+++ LSKD/download_weight/download_lskd_weight.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214859&authkey=ANZIVKNWJ4Gm9Z8" -O ../weights/resnet32x4_resnet8x4_LSKD_best.pth
+
+wget --no-check-certificate "https://onedrive.live.com/download?cid=B68D5C35DAE30358&resid=B68D5C35DAE30358%214860&authkey=AI5jYoMycuQUs4I" -O ../weights/vgg19_vgg11_LSKD_best.pth
+
